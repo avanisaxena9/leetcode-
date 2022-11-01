d3b1df77872b9088eb37947602c4d63f3c2c1433
Avani <55042694+avanisaxena9@users.noreply.github.com>
2022-11-01 18:52:13 +0530

    Create 1. Two Sum.java

---
+++ easy/1. Two Sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int count=0;
+        int[] a= new int[2];
+         for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if (nums[i]+nums[j]==target){
+                    
+                    a[0]=i;
+                    a[1]=j;
+                   count=1;
+                }
+                else 
+                    continue;
+            }
+        }
+        return a;
+    }
+}
