cbaaa569393c1eccf469f64751e1c1f3534b5d31
Avani Saxena <55042694+avanisaxena9@users.noreply.github.com>
2023-01-03 18:49:22 +0530

    Create 944. Delete Columns to Make Sorted.java

---
+++ easy/944. Delete Columns to Make Sorted.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int n=0;
+        for(int j=0 ; j<strs[0].length() ; j++) {
+            for( int i = 0; i<strs.length-1 ; i++ ) {
+                if ( !(strs[i].charAt(j) <= strs[i+1].charAt(j)))
+                {
+                    n++;
+                    break;
+                }
+            }            
+        }
+        return n;
+    }
+}
