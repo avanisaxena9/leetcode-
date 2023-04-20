607c4f873ec40604fbc540c67bd1e2621f1a2d76
Avani Saxena <55042694+avanisaxena9@users.noreply.github.com>
2023-04-20 19:41:58 +0530

    Update 20. Valid Parentheses.java

--- easy/20. Valid Parentheses.java
+++ easy/20. Valid Parentheses.java
@@ -15,3 +15,25 @@ class Solution {
 	return stack.isEmpty();
 }
     }
+
+
+// Alterante solution
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        int n = 0;
+
+        for(char ch : s.toCharArray()){
+            if (n==1) return false;
+            if (ch == '(') stack.push(')');
+             if (ch == '{') stack.push('}');
+              if (ch == '[') stack.push(']');
+              if(stack.isEmpty() && (ch == ')' || ch == '}' ||  ch == ']') ) return false;
+             if (ch == ')' && ch != stack.pop() ) return false;
+              if (ch == '}' && ch != stack.pop()) return false;
+               if (ch == ']' && ch != stack.pop()) return false;
+        }
+        return stack.isEmpty(); 
+    }
+}
