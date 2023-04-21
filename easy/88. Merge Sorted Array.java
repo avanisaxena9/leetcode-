358e9433b18d20b1bd7600276e8390eda59b2f98
Avani Saxena <55042694+avanisaxena9@users.noreply.github.com>
2023-04-21 14:32:23 +0530

    Update 88. Merge Sorted Array.java

--- easy/88. Merge Sorted Array.java
+++ easy/88. Merge Sorted Array.java
@@ -17,3 +17,15 @@ class Solution {
         }
     }
 }
+
+//Alternate solution 
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i = 0 ; i<nums2.length ; i++){
+            nums1[m] = nums2[i];
+            m++;
+        }
+        Arrays.sort(nums1);
+    }
+}
