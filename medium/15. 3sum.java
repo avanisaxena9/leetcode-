48fc8bc61f87cade08da11bba948abcb8cfa7690
Avani Saxena <55042694+avanisaxena9@users.noreply.github.com>
2023-05-28 19:04:48 +0530

    Create 15. 3sum.java

---
+++ medium/15. 3sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        Set<List<Integer>> ans=new HashSet<>();
+        for(int i = 0; i < nums.length-2; i++){
+            int p1 = i+1;
+            int p2 = nums.length-1;
+            while(p1 < p2){
+                int sum = nums[i]+nums[p1]+nums[p2];
+                if(sum == 0){
+                    
+                    ans.add(Arrays.asList(nums[i], nums[p1], nums[p2]));
+                    p1++;
+                }
+                else if(sum < 0){
+                    p1++;
+                }
+                else{
+                    p2--;
+                }
+            }
+        }
+        return new ArrayList<>(ans);
+    }
+}
